10f347df62ca55318dd7d21be7359fb6cfca4005
shawn0103 <spd5@hw.ac.uk>
2020-11-14 14:19:44 +0400

    Server

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var app = require('express')(); // Express frameork 
+var http = require('http').createServer(app);
+var io = require('socket.io')(http); // Socket io server
+
+app.get('/', (req, res) => { //If user sends get req to url
+  res.sendFile(__dirname + '/server.html'); // send user this file
+});
+
+io.on('connection', (socket) => { //listening for diff events 
+  console.log('a user connected');
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:4000');
+});
