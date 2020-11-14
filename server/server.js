29fddf5f45c5b902ad1faf774dc43a77624e6bae
shawn0103 <spd5@hw.ac.uk>
2020-11-14 14:51:28 +0400

    server

--- server/server.js
+++ server/server.js
@@ -3,13 +3,13 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http); // Socket io server
 
 app.get('/', (req, res) => { //If user sends get req to url
-  res.sendFile(__dirname + '/server.html'); // send user this file
+    res.sendFile(__dirname + '/server.html'); // send user this file
 });
 
 io.on('connection', (socket) => { //listening for diff events 
-  console.log('a user connected');
+    console.log('a user connected');
 });
 
 http.listen(3000, () => {
-  console.log('listening on *:4000');
+    console.log('listening on *:3000');
 });
