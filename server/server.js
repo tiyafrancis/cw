762f7b6447ee90f90dd2ba152752126e966dc682
Tiya Francis <t.2000@hw.ac.uk>
2020-11-25 12:27:14 +0400

    added sql query (untested)

--- server/server.js
+++ server/server.js
@@ -3,6 +3,16 @@ const path = require('path');
 const http = require('http');
 //const PORT = process.env.PORT || 3000  
 const socketio = require('socket.io'); // Socket io server
+var mysql = require('mysql'); // using mysql module
+
+// connect to db
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",    
+    password: "<password>", // enter password of mysql db
+    database: "pacman"
+}); 
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -11,11 +21,32 @@ app.use(express.static(path.join(__dirname, "client"))); //Serving static folder
 
 
 server.listen(3000, () => { // Listening on port 
-    console.log('listening on *:3000');
+    console.log('listening on port:3000');
 });
 var playerNames = {};
 const connections = []; // 5 players in each game 
 var playerStatus = {}; //Players score, location
+
+// not sure about the positioning of the below query
+con.connect(function(err) {
+    if (err) 
+        console.log("Can not connect to db!");
+    else
+    {
+        console.log("Connected!");
+        con.query("CREATE DATABASE pacman", function (err, result) {
+            if (err) throw err;
+            console.log("Database created");
+        });
+        var sql = "CREATE TABLE scoreboard (players VARCHAR(255), score int)";
+        con.query(sql, function (err, result) {
+            if (err) throw err;
+            console.log("Table created");
+        });
+       
+    }
+        
+});
 
 io.on('connection', socket => { //On user connection
     
@@ -27,13 +58,20 @@ io.on('connection', socket => { //On user connection
         }
     }
 */
-
     socket.on('userName', name => {
         socket.id = name;
         console.log(`yay!! ${name} just connected!`);
     })
 
-    socket.on('disconnect', () => {
+    socket.on('disconnect', data => {
+        // not sure about the positioning of the below query
+        // the idea is: on disconnection, 
+        // store the player name and score so it can accessed later
+        con.query((`INSERT INTO customers (name, address) VALUES (${socket.id}, ${data.playerScore})`), function (err, result) {
+            if (err) throw err;
+            console.log("1 record inserted");
+          });
+
         console.log(`aww ${socket.id} just left`);
     })
 
@@ -55,14 +93,11 @@ io.on('connection', socket => { //On user connection
         console.log(playerStatus);
     })
 
-
-
     // TODO: setInterval function 
 
     setInterval(() => { // Refreshing wvery 2 milliseconds 
         socket.emit('updateScores', playerStatus);
     }, 300)
-
 
     /*
     socket.emit('player-number', playerNumber); //Telling the user what player they are
@@ -79,3 +114,4 @@ io.on('connection', socket => { //On user connection
     // }
     
 });
+
